015fb22c23afc9858da44f26f01d5d469f6e6174
Torsten Giess <torsten.giess@ibtb.uni-stuttgart.de>
2023-07-08 12:10:40 +0200

    Create csvreader.py and update poetry

---
+++ input/csvreader.py
@@ -0,0 +1,35 @@
+"""Tool for reading the CSV room plan for the Bad Boll Seminaris."""
+
+import os
+from pathlib import Path
+
+import pandas as pd
+
+
+SAMPLE_PATH = (
+    Path.cwd() / "data/seminaris-hotel-badboll-conference-factsheet-A4.pdf"
+)
+
+
+def extract_floor_plan(
+    path_to_csv: Path | os.PathLike | str | bytes = None,
+) -> pd.DataFrame:
+    """Parse the CSV file containing relevant rooms and dimensions and
+    return it as a `pandas.DataFrame`.
+
+    Args:
+        path_to_csv (Path | os.PathLike | str | bytes, optional): Pointer to the CSV file or stream. Defaults to None.
+
+    Returns:
+        pd.DataFrame: Content of the CSV file as dataframe.
+    """
+    if not path_to_csv:
+        path_to_csv = Path.cwd() / "data/relevant_rooms.csv"
+    df = pd.read_csv(
+        filepath_or_buffer=path_to_csv,
+    )
+    return df
+
+
+if __name__ == "__main__":
+    print(extract_floor_plan())

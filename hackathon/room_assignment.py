931f30abc6cf15b3bec6c8421d2e39f654988e15
Kalimer <rosin1991@googlemail.com>
2023-07-08 13:35:35 +0200

    adding notes/todos

--- hackathon/room_assignment.py
+++ hackathon/room_assignment.py
@@ -1,4 +1,9 @@
 import numpy as np
+from pandas_ods_reader import read_ods
+
+def get_data(path):
+    data = read_ods(path)
+    return data
 
 def create_matrix(n):
     b = np.random.randint(0,2,size=(n,n))
@@ -20,9 +25,19 @@ def find_pairs(A):
                 matching_pair.append((row,column))
     return matching_pair
 
-
+input_file = '../data/poster_registrations.ods'
+data = get_data(input_file)
+print(data)
 n = 6
 A = create_matrix(n)
 print(A)
 matching_pair = find_pairs(A)
 print(matching_pair)
+
+#TODO
+# place one poster
+# check this posters score with all the others
+# do a ranking
+# build a cluster with maximum score
+#   first take all the score two
+#   then take score ones to fill up, if there are spots left

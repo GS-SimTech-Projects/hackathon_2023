894abfa81db2214157e795f76b67dad96ddce34f
ac130513 <julia.pelzer@ipvs.uni-stuttgart.de>
2023-07-08 11:46:22 +0200

    creating similarity matrix from keywords in csv

--- hackathon/time_assignment/create_similarity_matrix.py
+++ hackathon/time_assignment/create_similarity_matrix.py
@@ -1,18 +1,19 @@
-import csv
 import numpy as np
-import os
+from utils import read_csv
 
-def create_similarity_matrix(file:str = "sample_inputs/poster_data.csv"):
-    # read csv file
-    with open(file, newline='') as f:
-        reader = csv.reader(f)
-        data = list(reader)
-
+def create_similarity_matrix(data:np.ndarray):
     # reduce data to only the columns we need
     # header = data[0]
-    data = np.array(data[1:])
+    data = data[1:]
     data = data[:, -3:]
     data[:,0] = np.arange(data.shape[0])
+
+    matrix = np.zeros((data.shape[0], data.shape[0]))
+    for i in range(data.shape[0]):
+        for j in range(data.shape[0]):
+            matrix[i, j] = similarity(data[i], data[j])
+    assert matrix[i,i] == 2
+    return matrix
 
 def similarity(data1:np.ndarray, data2:np.ndarray):
     distance = 0
@@ -22,5 +23,15 @@ def similarity(data1:np.ndarray, data2:np.ndarray):
 
     return distance
 
+def test_matrix():
+    data = read_csv()
+    matrix = create_similarity_matrix(data)
+    for i in range(data.shape[0]-1):
+        assert matrix[i,i] == 2, f"matrix values on diagonal should always be 2"
+
 if __name__=="__main__":
-    create_similarity_matrix()
+    data = read_csv()
+    matrix = create_similarity_matrix(data)
+    print(matrix)
+    test_matrix()
+
---
+++ hackathon/time_assignment/utils.py
@@ -0,0 +1,13 @@
+import csv
+import numpy as np
+from typing import List
+import matplotlib.pyplot as plt
+
+def read_csv(file:str = "sample_inputs/poster_data.csv"):
+    """
+    Reads a csv file and returns a list of lists"""
+    with open(file, newline='') as f:
+        reader = csv.reader(f)
+        data = list(reader)
+    return np.array(data)
+
